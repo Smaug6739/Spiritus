f6dc99ce9e667088fb72e08e13d3ca003927d629
raphael veauville <veauville.raphael@gmail.com>
2020-05-15 12:42:05 +0200

    Update unmute.js

--- commands/Moderation/unmute.js
+++ commands/Moderation/unmute.js
@@ -1,5 +1,4 @@
 const { MessageEmbed } = require("discord.js");
-
 module.exports.run = (client, message, args) => {
   let user = message.guild.member(message.mentions.users.first());
   let muteRole = message.guild.roles.cache.find(r => r.name === 'muted');
