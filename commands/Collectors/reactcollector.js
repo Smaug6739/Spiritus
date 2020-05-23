f28d6e06f8f8917b1bb1b2c18012188dcbdec0da
raphael veauville <veauville.raphael@gmail.com>
2020-05-23 17:37:50 +0200

    Create folders collectors

---
+++ commands/Collectors/reactcollector.js
@@ -0,0 +1,30 @@
+const { MESSAGES } = require("../../util/constants");
+
+module.exports.run = (client, message, args) => {
+  const filter = reaction => reaction.emoji.name === '⭐';
+  // const collector = message.createReactionCollector(filter, { time: 10000 });
+
+  message.react('⭐')
+    .then(() => {
+      message.awaitReactions(filter, { time: 10000 })
+        .then(collected => message.channel.send(`${collected.size} réaction collectée.`));
+    });
+
+  // collector.on('end', collected => {
+  //   message.channel.send(`${collected.size} réaction collectée.`);
+  // });
+};
+
+
+module.exports.help = {
+    name: "collector-reac",
+    aliases: ['collector-reac','col-rec'],
+    category: 'collectors',
+    description: "Renvoie une liste de commandes ou les informations sur une seule!",
+    cooldown: 3,
+    usage: '<command_name>',
+    exemple :["help","help ping"],
+    isUserAdmin: false,
+    permissions: false,
+    args: false
+  };
