53bb8c128d4064eb7ed324dfcb002bb2a6e2d722
raphael veauville <veauville.raphael@gmail.com>
2020-05-23 16:56:06 +0200

    Fix help.js

--- commands/Misc/help.js
+++ commands/Misc/help.js
@@ -2,11 +2,12 @@ const { MessageEmbed } = require("discord.js");
 const { readdirSync } = require("fs");
 const categoryList = readdirSync('./commands');
 module.exports.run = (client, message, args, settings) => {
-    
+  let { LOGOROUGE } = require('../../configstyle');
+
     if (!args.length) {
       const embed = new MessageEmbed()
         .setColor("#62b02e")
-        .setTitle(`<:15896389126821:711223108866015242> **Liste  des commandes :** `)
+        .setTitle(`${LOGOROUGE} **Liste  des commandes :** `)
 
         .addField("Liste des commandes", `Une liste de toutes les sous-catégories disponibles et leurs commandes.\nPour plus d'informations sur une commande, tapez \`${settings.prefix}help <command_name>\`.`)
         .setTimestamp()
@@ -25,12 +26,11 @@ module.exports.run = (client, message, args, settings) => {
       const command = client.commands.get(args[0]) || client.commands.find(cmd => cmd.help.aliases && cmd.help.aliases.includes(args[0]));
       console.log(command);
       if (!command) return message.reply("cette commande n'existe pas!");
-      const logo = '<:15896389126821:711223108866015242>';
       const embed = new MessageEmbed()
       
         .setColor("#62b02e")
         //.setTitle(`\`${settings.prefix}${command.help.name}\``)
-        .setTitle(`${logo} **Commande :** ${settings.prefix}${command.help.name}`)
+        .setTitle(`${LOGOROUGE} **Commande :** ${settings.prefix}${command.help.name}`)
         .addField("Description :", `${command.help.description} (cd: ${command.help.cooldown}secs)`)
         .addField("Utilisation :", command.help.usage ? `${settings.prefix}${command.help.name} ${command.help.usage}` : `${settings.prefix}${command.help.name}`, true)
         .setTimestamp()
