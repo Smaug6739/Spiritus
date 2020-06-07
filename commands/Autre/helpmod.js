36caeb3287bb8ed80e85708c66c259dc0dd05f86
raphael veauville <veauville.raphael@gmail.com>
2020-06-07 10:44:43 +0200

    Update helpmod.js

--- commands/Autre/helpmod.js
+++ commands/Autre/helpmod.js
@@ -1,19 +1,19 @@
 const { MessageEmbed } = require("discord.js");
 const { readdirSync } = require("fs");
 const categoryList = readdirSync('./commands');
+const {LOGOBOT} = require('./../../configstyle')
 module.exports.run = (client, message, args) => {
     
     if (!args.length) {
       const embed = new MessageEmbed()
         .setColor("#62b02e")
-        
-        .addField("Liste des commandes", `Une liste de toutes les sous-catégories disponibles et leurs commandes.\nPour plus d'informations sur une commande, tapez \`${client.config.PREFIX}help <command_name>\`.`)
+        .addField(`${LOGOBOT}Liste des commandes`, `Une liste de toutes les sous-catégories disponibles et leurs commandes.\nPour plus d'informations sur une commande, tapez \`${client.config.PREFIX}help <command_name>\`.`)
         .setTimestamp()
         .setFooter('BOT ID : 689210215488684044', `${message.guild.iconURL()}`);
       //for (const category of categoryList) {
           const category = "moderation"
         embed.addField(
-          `${category}`,
+          `${category.replace('m','M')}`,
           `${client.commands.filter(cat => cat.help.category === category.toLowerCase())
             .map(cmd => '**'+`${client.config.PREFIX}`+cmd.help.name +' ** - '+ cmd.help.description).join(`\r\n`)}`
         );
@@ -24,12 +24,12 @@ module.exports.run = (client, message, args) => {
   
   module.exports.help = {
     name: "helpmod",
-    aliases: ['helpmod','cmdsmod'],
+    aliases: ['helpmod','cmdsmod','help-mod'],
     category: 'autre',
     description: "Renvoie une liste de commandes ou les informations sur une seule!",
     cooldown: 3,
     usage: '<command_name>',
-    exemple :["help","help prune"],
+    exemple :["help-mod","help prune"],
     isUserAdmin: false,
     permissions: false,
     args: false
