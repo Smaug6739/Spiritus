3accbbc7e1afa09183e07c63f7683d6573df416f
raphael veauville <veauville.raphael@gmail.com>
2020-06-08 18:35:34 +0200

    Add git pull command

--- commands/Admin/pull.js
+++ commands/Admin/pull.js
@@ -6,13 +6,15 @@ module.exports.run = async (client, message, args) =>{
     let loading = '<a:loading:688692468195262475>'
     if(!ADMIN.includes(message.author.id)) return message.channel.send(`${FALSE}Tu n'est pas admin du BOT `)
     console.log("Redemarage")
-
-    try {
-        await exec('gitt pull');
-        message.channel.send(`${TRUE} Updated.`);
-    } catch (err) {
-        message.channel.send(`${FALSE} An error occured:\n\`\`\`${err}\n\`\`\``);
-    }
+    message.channel.send(`${loading} Commande en cour d'execution...`).then(async msg =>{
+        try {
+            await exec('git pull');
+            msg.edit(`${TRUE} Updated.`);
+        } catch (err) {
+            msg.edit(`${FALSE} An error occured:\n\`\`\`${err}\n\`\`\``);
+        }
+    })
+    
 
 }
 
