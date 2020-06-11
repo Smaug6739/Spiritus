5a1034a4becad25f0b0205c75a5438693c708154
raphael veauville <veauville.raphael@gmail.com>
2020-06-11 14:07:02 +0200

    Update botinfo.js

--- commands/Misc/botinfo.js
+++ commands/Misc/botinfo.js
@@ -5,6 +5,7 @@ module.exports.run = (client, message, args) => {
     .setColor(`${EMBED}`)
     .setAuthor(`${client.user.username} Info`, client.user.avatarURL())
     .addFields(
+      { name: 'Developpeur', value: `Smaug#6739`, inline: true },
       { name: 'Mémoire', value: `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`, inline: true },
       { name: 'Uptime', value: `${Math.floor(client.uptime / 1000 / 60).toString()} minutes`, inline: true },
       //{ name: '\u200b', value: `\u200b`, inline: true },
@@ -12,7 +13,11 @@ module.exports.run = (client, message, args) => {
       { name: 'Salons', value: `${client.channels.cache.size.toString()}`, inline: true },
       { name: 'Utilisateurs', value: `${client.guilds.cache.map(g => g.memberCount).reduce((a, b) => a + b)}`, inline: true },
       { name: 'Version', value: `1.0.0`, inline: true },
-      { name: 'Support', value: `[Serveur support ](https://discord.gg/TC7Qjfs)`, inline: true }
+      { name: 'Librairie ', value: `discord.js (javascript)`, inline: true },
+
+      { name: 'Support', value: `[Serveur support ](https://discord.gg/TC7Qjfs)`, inline: true },
+      { name: 'Invitation :', value: `[Invite](https://discordapp.com/oauth2/authorize?client_id=689210215488684044&scope=bot&permissions=1946446974)`, inline: true }
+
     );
 
 		message.channel.send(embed);
