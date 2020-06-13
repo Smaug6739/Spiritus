93cda0e9fdfe15f6062466ff58789d35c4284933
raphael veauville <veauville.raphael@gmail.com>
2020-06-13 10:08:29 +0200

    Add logs guildCreate and logs guildDelete

--- events/guild/guildCreate.js
+++ events/guild/guildCreate.js
@@ -1,8 +1,25 @@
+const {MessageEmbed} = require('discord.js')
+const {VERTCLAIRE} = require('./../../configstyle')
 module.exports = async (client, guild) => {
  /* const newGuild = {
     guildID: guild.id,
     guildName: guild.name
   };
  await client.createGuild(newGuild)*/
- console.log(`Je suis sur un nouveau serveur : ${guild.name}`)
+ //console.log(`Je suis sur un nouveau serveur : ${guild.name}`)
+ const embed = new MessageEmbed()
+ .setAuthor('Ajouter sur un nouveau serveur :')
+ .setTitle(`${guild.name}`)
+ .setThumbnail(`${guild.iconURL()}`)
+ .setColor(`#0x00FF00`)
+ .addFields(
+  { name: 'Nom', value: `${guild.name}\n(\`${guild.id}\`)`, inline: true },
+  { name: 'Owner', value: `${guild.owner.user.tag}\n(\`${guild.ownerID}\`)`, inline: true },
+  { name: 'Region', value: `${guild.region.toUpperCase()}`, inline: true },
+  { name: 'Members :', value: `${guild.memberCount}`, inline: true },
+ )
+ .setTimestamp()
+ .setFooter('Spiritus bot logs | BOT ID : 689210215488684044')
+ client.channels.cache.get('721270980823220324').send(embed)
+
 };
