d8036144686b7dd04d7ae7b6bc9013ebf84f96a6
raphael veauville <veauville.raphael@gmail.com>
2020-06-23 21:22:43 +0200

    Update configstyle comments

--- configstyle.js
+++ configstyle.js
@@ -2,7 +2,7 @@ exports.ADMIN = ["611468402263064577","452222720428605440"]
 //smaug //noval
 
 exports.VERTCLAIRE = '#35f092';
-exports.EMBED = '#222427';//HELP;
+exports.EMBED = '#222427';//HELP;COMMAND.JS;
 exports.ROSE = '#fd7aff';//LOGS;
 exports.ORANGE = '#ffa500';//TICKETS,KICK,MUTE,UNMUTE
 exports.ROUGE = '#dc143c';//MEMBER_REMOVE,BAN,PURGE,PRUNE,UNBAN
