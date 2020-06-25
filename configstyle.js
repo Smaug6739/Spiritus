81558fa18c8fa6cac664c3f41889d2244912b553
raphael veauville <veauville.raphael@gmail.com>
2020-06-25 17:58:27 +0200

    Update owners bot

--- configstyle.js
+++ configstyle.js
@@ -1,5 +1,5 @@
-exports.ADMIN = ["611468402263064577","452222720428605440"]
-//smaug //noval
+exports.ADMIN = ["611468402263064577"]
+//smaug
 
 exports.VERTCLAIRE = '#35f092';
 exports.EMBED = '#222427';//HELP;COMMAND.JS;
@@ -30,4 +30,4 @@ exports.ETIQUETTE = ' <:X6etiquetteX6:713455416289525851>';
 exports.BOOST = '<:X9nitro4X9:713120475487797340>';
 exports.NSFW = ' <:X3channel_nsfwX3:713120251109179393>';
 
-exports.FLECHE = '<:fleche:721288083974389842>';
+exports.FLECHE = '<:fleche:721288083974389842>';//Commandes d'explication
