1cc299b7da3d4be72890b87abccef9c39cc7f4d9
raphael veauville <veauville.raphael@gmail.com>
2020-06-30 10:37:51 +0200

    Update

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     TOKEN: "Njg5MjEwMjE1NDg4Njg0MDQ0.XtfkIg.vuFIrI3A7wBsJSiwwBiBgZSQuI8",
     PREFIX: "?",
     //DBCONNECTION: "mongodb+srv://spiritus:oHBDysvqARAg1chL@cluster0-y7doh.mongodb.net/test?retryWrites=true&w=majority",
-    DBCONNECTION : `mongodb+srv://vps:HhvnmdzfyvjXBLdu@cluster0-y7doh.mongodb.net/<dbname>?retryWrites=true&w=majority`,
+    DBCONNECTION : `mongodb+srv://vps:HhvnmdzfyvjXBLdu@cluster0-y7doh.mongodb.net/test?retryWrites=true&w=majority`,
     ADMIN : ["611468402263064577"],
     DEFAULTSETTINGS: {
       prefix: "?",
