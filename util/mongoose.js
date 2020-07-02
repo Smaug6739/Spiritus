41ec0e5e24b8a8035c2a26def62e4beef30f0732
raphael veauville <veauville.raphael@gmail.com>
2020-07-02 18:27:33 +0200

    Update mongoose.js

--- util/mongoose.js
+++ util/mongoose.js
@@ -9,14 +9,14 @@ module.exports = {
       useUnifiedTopology: true,
       useCreateIndex: true,
       useFindAndModify: false,
-      /*autoIndex: false, // Don't build indexes
+      autoIndex: false, // Don't build indexes
       poolSize: 10, // Maintain up to 10 socket connections
       serverSelectionTimeoutMS: 10000, // Keep trying to send operations for 5 seconds
       socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-      family: 4 // Use IPv4, skip trying IPv6*/
+      family: 6 // Use IPv4, skip trying IPv6
     }
 
-    mongoose.createConnection(DBCONNECTION, mongOptions);//createConnection //connect
+    mongoose.connect(DBCONNECTION, mongOptions);//createConnection //connect
     mongoose.Promise = global.Promise;
     mongoose.connection.on("connected", () =>{ 
       console.log("Mongoose est connecté!")
