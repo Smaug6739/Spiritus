a78188db5f31c1c0a851e17e28fdc6174a87700f
raphael veauville <veauville.raphael@gmail.com>
2020-07-08 10:35:42 +0200

    Update

--- commands/Misc/ping.js
+++ commands/Misc/ping.js
@@ -1,9 +1,7 @@
-const {MessageEmbed} = require('discord.js')
-const util = require('util');
 module.exports.run =async(client, message, args) => {
 
     let debut = Date.now();
-    message.channel.sendd('Pong !').then(async(m) => await m.edit(`Pong  BOT : \`${Date.now()-debut}ms\` API : \`${client.ws.ping}ms\``));
+    message.channel.send('Pong !').then(async(m) => await m.edit(`Pong  BOT : \`${Date.now()-debut}ms\` API : \`${client.ws.ping}ms\``));
 
 
     /*let evaled;
