4ec5b0b628f1245982d2484f2c3528e4d5ad5b9d
raphael veauville <veauville.raphael@gmail.com>
2020-07-09 16:46:35 +0200

    Update

--- commands/Misc/exp.js
+++ commands/Misc/exp.js
@@ -57,10 +57,10 @@ module.exports.run = async (client, message, args, settings, dbUser) => {
                 ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
                 ctx.strokeStyle = '#74037b';
                 ctx.strokeRect(0, 0, canvas.width, canvas.height);
-                ctx.font = '30px sans-serif';
+                ctx.font = '28px sans-serif';
                 ctx.fillStyle = '#ffffff';
                 //ctx.fillText(`Rank du serveur,${member.displayName}!`, canvas.width / 2.8, canvas.height / 3.5);
-                ctx.fillText(`Cette personne n'a\npas de rank. Pour en gagner\n postez des messages !`, canvas.width / 2.6, canvas.height / 3);
+                ctx.fillText(`Cette personne n'a pas\n de rank. Pour en gagner\n postez des messages !`, canvas.width / 2.6, canvas.height / 3);
                 ctx.fillStyle = '#ffffff';
                 ctx.beginPath();
                 ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
