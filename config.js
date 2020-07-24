05690c78670d276759365ff4a6645c471f6d37f5
raphael veauville <veauville.raphael@gmail.com>
2020-07-24 10:49:32 +0200

    Update functions

---
+++ config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  PREFIX: ";",
+  owner: {
+    id: "611468402263064577",
+    username: "Smaug#6739"
+  },
+  ORIGINPULL: 'master',
+  ADMIN: ["611468402263064577"],
+  DEFAULTSETTINGS: {
+    prefix: ";",
+    logChannel: "bot-logs",
+    welcomeMessage: "Welcome {{user}}",
+    rankcard: "https://cdn.discordapp.com/attachments/715219402215129088/731041731159785482/rank_1.png"
+  },
+
+  emojis: {
+    ONLINE: '<:Z2Status1Z2:713121114725023885>',
+    IDLE: '<:Z1Status2Z1:713121099537448962>',
+    DND: '<:Y9Status3Y9:713121040485842984>',
+    OFFLINE: '<:Y7Status5Y7:713121039931932714>',
+    CHANNEL: '<:X5channelX5:713120296697331782>',
+    CHANNELNSFW: '<:X3channel_nsfwX3:713120251109179393>',
+    VOICE: '<:X2voiceX2:713120217118539879>',
+    ETIQUETTE: '<:X6etiquetteX6:713455416289525851>',
+    TRUE: '<a:Z9Z9:712562703310258228>',
+    FALSE: '<a:Z8Z8:712562703318646844>',
+    LOADING: '<a:loading:688692468195262475>',
+    BOOST: '<:X9nitro4X9:713120475487797340>',
+    FLECHE: '<:fleche:721288083974389842>',
+    LOGOBOT: '<:Z6158981175244605Z6:713121641701572698>',
+
+    success: '<a:Z9Z9:712562703310258228>',
+    error: '<a:Z8Z8:712562703318646844>',
+    loading: '<a:loading:688692468195262475>',
+    fleche: '<:fleche:721288083974389842>',
+
+  },
+  color: {
+    EMBEDCOLOR: '#d91414',
+    ROUGE: '#ff0004',
+    ORANGE: '#ff8400',
+    VERT: '#02ff30',
+  },
+}
--- util/functions.js
+++ util/functions.js
@@ -1,32 +1,31 @@
 const mongoose = require("mongoose");
-const { Guild,User } = require("../models/index");
+const { Guild, User } = require("../models/index");
 
 module.exports = client => {
-  
+
   client.createGuild = async guild => {
     const merged = Object.assign({ _id: mongoose.Types.ObjectId() }, guild);
     const createGuild = await new Guild(merged);
-   await createGuild.save().then(g => console.log(`Nouveau serveur -> ${g.guildName}`));
+    await createGuild.save().then(g => console.log(`Nouveau serveur -> ${g.guildName}`));
 
   };
   client.deleteGuild = async guild => {
     const data = await Guild.findOne({ guildID: guild.id });
-    if(data){
+    if (data) {
       await data.delete()
     }
-    
+
   };
-  //const { DEFAULTSETTINGS: defaultSettings} = require(`../config.js`);
   client.getGuild = async guild => {
     const data = await Guild.findOne({ guildID: guild.id });
-    if (!data){ 
+    if (!data) {
       const newGuild = {
-      guildID: guild.id,
-      guildName: guild.name
-      
+        guildID: guild.id,
+        guildName: guild.name
+
       };
 
-    await client.createGuild(newGuild)
+      await client.createGuild(newGuild)
     }
     if (data) return data;
     return client.config.DEFAULTSETTINGS;
@@ -44,78 +43,43 @@ module.exports = client => {
   }
 
 
-//----------------------------USERS-----------------------------------------------
+  //----------------------------USERS-----------------------------------------------
 
-client.createUser = async user => {
-  const merged = Object.assign({ _id: mongoose.Types.ObjectId() }, user);
-  const createUser = await new User(merged);
-  createUser.save().then(u => console.log(`Nouvel utilisateur -> ${u.username}`));
-};
+  client.createUser = async user => {
+    const merged = Object.assign({ _id: mongoose.Types.ObjectId() }, user);
+    const createUser = await new User(merged);
+    createUser.save().then(u => console.log(`Nouvel utilisateur -> ${u.username}`));
+  };
 
-client.getUser = async (user, guild) => {
-  const data = await User.findOne({ userID: user.id, guildID: guild });
-  
-  if (data) return data;
-  else return;
-};
-client.getUsers = async guild => {
-  const data = await User.find({guildID : guild.id});
-  
-  if (data) return data;
-  else return;
-};
+  client.getUser = async (user, guild) => {
+    const data = await User.findOne({ userID: user.id, guildID: guild });
 
-client.updateUser = async (user, settings) => {
-  let data = await client.getUser(user, user.guild.id);
-  if (typeof data !== "object") data = {};
-  for (const key in settings) {
-    if (data[key] !== settings[key]) data[key] = settings[key];
+    if (data) return data;
+    else return;
+  };
+  client.getUsers = async guild => {
+    const data = await User.find({ guildID: guild.id });
+
+    if (data) return data;
+    else return;
+  };
+
+  client.updateUser = async (user, settings) => {
+    let data = await client.getUser(user, user.guild.id);
+    if (typeof data !== "object") data = {};
+    for (const key in settings) {
+      if (data[key] !== settings[key]) data[key] = settings[key];
+    }
+    return data.updateOne(settings);
+  };
+  client.addExp = async (client, member, exp) => {
+    const userToUpdate = await client.getUser(member, member.guild.id);
+    const updateExp = userToUpdate.experience + exp;
+    await client.updateUser(member, { experience: updateExp });
   }
-  return data.updateOne(settings);
-};
-client.addExp = async (client, member, exp)=>{
-  const userToUpdate = await client.getUser(member, member.guild.id);
-  const updateExp = userToUpdate.experience + exp;
-  await client.updateUser(member, {experience : updateExp});
-}
-client.removeExp = async (client, member, exp)=>{
-  const userToUpdate = await client.getUser(member, member.guild.id);
-  const updateExp = userToUpdate.experience - exp;
-  await client.updateUser(member, {experience : updateExp});
-}
-
-
-//-----------------------------CMD--------------------------------------------
-/*client.createCmd = async cmd => {
-  const merged = Object.assign({ _id: mongoose.Types.ObjectId() }, cmd);
-  const createCmd = await new Cmd(merged);
-  createCmd.save().then(u => console.log(`Nouvel commande -> ${u.nom}`));
-};
-client.getCmd = async (cmdNom, guild) => {
-  const data = await Cmd.findOne({ nom: cmdNom, guildID: guild.id });
-  if (data) return data;
-  else return ;
-};
-client.getCmds = async guild => {
-  const data = await Cmd.find({guildID : guild.id});
-  if (data) return data;
-  else return;
-};
-client.updateCmd = async (cmdNom,guild, settings) => {
-  let data = await client.getCmd(cmdNom, guild);
-  if (typeof data !== "object") data = {};
-  for (const key in settings) {
-    if (data[key] !== settings[key]) data[key] = settings[key];
+  client.removeExp = async (client, member, exp) => {
+    const userToUpdate = await client.getUser(member, member.guild.id);
+    const updateExp = userToUpdate.experience - exp;
+    await client.updateUser(member, { experience: updateExp });
   }
-  if(data.nom){
-    console.log(data)
-  return data.updateOne(settings);
-  }
-}
-client.deleteCmd = async (cmdNom, guild) => {
-  const data = await Cmd.findOne({ nom: cmdNom, guildID: guild.id });
-  if(data){
-    await data.delete()
-  }
-};*/
 };
