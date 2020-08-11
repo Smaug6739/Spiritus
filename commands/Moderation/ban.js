d6bff7f980dbad262e114e6216b29e370e858ef2
raphael veauville <veauville.raphael@gmail.com>
2020-08-11 10:39:42 +0200

    up

--- commands/Moderation/ban.js
+++ commands/Moderation/ban.js
@@ -14,7 +14,7 @@ module.exports.run = async (client, message, args, settings) => {
     }
     //if(!args[1])return message.channel.send(`${client.config.emojis.FALSE} Merci d'indiquer le contenu d'un message.`)
     let query;
-    let reason = 'No reason provided';
+    let reason = 'Aucune raison';
     query = args.slice(1).join(' ')
     reason = encodeURIComponent(reason);
     const msgs = await message.channel.messages.fetch()
