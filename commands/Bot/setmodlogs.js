8f41b058a18c3e26ea833434fd60e8b95607082c
raphael veauville <veauville.raphael@gmail.com>
2020-08-25 20:28:31 +0200

    Update

--- commands/Bot/setmodlogs.js
+++ commands/Bot/setmodlogs.js
@@ -1,13 +1,12 @@
-const {MessageEmbed} = require('discord.js')
 module.exports.run = async (client, message, args,settings) => {
-    if(!message.member.hasPermission('MANAGE_GUILD'))return message.channel.send(`${client.config.emojis.error}Vous devez avoir la permission de gérer le serveur pour utiliser cette commande.`)
-        const channel = await client.resolveChannel(message.guild, args[0])
-        if(channel){
-            client.updateGuild(message.guild, {modLogs : channel.id} )
-            message.channel.send(`${client.config.emojis.success}Le salon de logs est maintanant <#${channel.id}> .`)
-        }else{
-            message.channel.send(`${client.config.emojis.error}Je n'ai pas trouvé ce salon.`)
-        }
+if(!message.member.hasPermission('MANAGE_GUILD'))return message.channel.send(`${client.config.emojis.error}Vous devez avoir la permission de gérer le serveur pour utiliser cette commande.`)
+    const channel = await client.resolveChannel(message.guild, args[0])
+    if(channel){
+        client.updateGuild(message.guild, {modLogs : channel.id} )
+        message.channel.send(`${client.config.emojis.success}Le salon de logs est maintanant <#${channel.id}> .`)
+    }else{
+        message.channel.send(`${client.config.emojis.error}Je n'ai pas trouvé ce salon.`)
+    }
     
 }
 module.exports.help = {  
