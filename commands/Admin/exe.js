32d44cfe2f7a8f48cfcfe44e3fc75a1b31278cf4
raphael veauville <veauville.raphael@gmail.com>
2020-08-26 09:27:28 +0200

    up

--- commands/Admin/exe.js
+++ commands/Admin/exe.js
@@ -2,8 +2,8 @@ const util = require('util');
 const child_process = require('child_process');
 const exec = util.promisify(child_process.exec);
 module.exports.run = async (client, message, args) =>{
-    /*if(!client.config.ADMIN.includes(message.author.id)) return message.channel.send(`${client.config.emojis.FALSE}Tu n'est pas admin du BOT `)
-    console.log("Execution d'une commande")
+    if(!client.config.ADMIN.includes(message.author.id)) return message.channel.send(`${client.config.emojis.error}Tu n'est pas admin du BOT `)
+    /*console.log("Execution d'une commande")
     message.channel.send(`${client.config.emojis.loading} Commande en cour d'execution...`).then(async msg =>{
         try {
             await exec(`${args.join(" ")}`);
