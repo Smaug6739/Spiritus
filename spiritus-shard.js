d31dd460f00b6d02dd3dc27318d6a80eaa79c055
raphael veauville <veauville.raphael@gmail.com>
2020-09-04 14:37:20 +0200

    Update

--- spiritus-shard.js
+++ spiritus-shard.js
@@ -1,6 +1,9 @@
 const { ShardingManager } = require('discord.js');
-const {TOKEN} = require('./config')
-const manager = new ShardingManager('./main.js', { token: `${TOKEN}` });
+const CONFIG = require('./configuration')
+const manager = new ShardingManager('./main.js', { token: `${CONFIG.TOKENS.DISCORD}` });
 
 manager.spawn();
-manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
+manager.on('shardCreate', shard => {
+    console.log(`Launched shard ${shard.id}`)
+    //console.log(shard)
+});
