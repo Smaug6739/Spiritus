e9ba8a154b6e0526f9bb762ce3f98d459063ae61
raphael veauville <veauville.raphael@gmail.com>
2020-09-19 10:00:39 +0200

    up

--- events/client/ready.js
+++ events/client/ready.js
@@ -26,4 +26,29 @@ module.exports =async client => {
     avatarURL: `${client.configuration.WEBHOOKS.CONNECTIONS.DISCORD.AVATAR}`,
     embeds: [embed],
   });
+
+
+  if(client.user.id === client.config.IDOFFITIEL){
+    const dbl = new DBL("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4OTIxMDIxNTQ4ODY4NDA0NCIsImJvdCI6dHJ1ZSwiaWF0IjoxNTk4MDI4MzE5fQ.Mcm1t7ehWiS1daU8RW38B2ebwsQPpz3ivsXAC-C0cvc", {
+      webhookPort: 5000, 
+      webhookAuth: 'password' 
+  }, client);
+  dbl.webhook.on('ready', hook => {
+      console.log(`Webhook running at http://${hook.hostname}:${hook.port}${hook.path}`);
+    });
+  dbl.webhook.on('vote', async vote => {
+      const guild = client.guilds.cache.get('710759353472516176')
+      const channel = client.channels.cache.get(`736074693790138409`)
+      try{
+          const member = guild.members.cache.get(vote.user);
+              channel.send(`<@!${member.id}> a voté pour Spiritus sur DBL ! Merci ❤️`)
+      
+  
+      }catch(err){
+          console.log(err)
+          const channelerror = client.channels.cache.get(`730120455243038794`)
+          channelerror.send(`**Erreur DBL : **\n\`\`\`js\n${err}\`\`\``)
+      }
+  })
+  }
   }
