838f86a645e458a8d2aa33a2d255c5533aa86d9f
raphael veauville <veauville.raphael@gmail.com>
2020-09-26 14:52:31 +0200

    Add reaction roles module

--- events/reactions/messageReactionAdd.js
+++ events/reactions/messageReactionAdd.js
@@ -1,15 +1,37 @@
 module.exports = async (client, messageReaction, user) => {
+  const message = messageReaction.message;
+  const member = message.guild.members.cache.get(user.id);
+  const emoji = messageReaction.emoji.name;
+  const emojiID = messageReaction.emoji.id;
+  const channel = message.guild.channels.cache.find(c => c.id === '708593997890977823');
+  const roleun = message.guild.roles.cache.get("713757081966215269");
+  const roledeux = message.guild.roles.cache.get("713757111678664845");
+  if (member.user.bot) return;
+  const settings = await client.getGuild(message.guild);
 
-  //console.log('RR add')
-  /*const guild = messageReaction.message.channel.guild
+      settings.reactionroles.forEach(element => {
+
+       if(element.messageID === `${message.id}` && element.channelID === `${message.channel.id}`){
+         if(element.emoji == `${emojiID}`|| element.emoji == `${emoji}`){
+          //console.log(element)
+            let roleToAdd = message.guild.roles.cache.get(`${element.roleID}`)
+            if(member.roles.highest.comparePositionTo(roleToAdd) <= 0)return console.log(`Vous ne pouvez pas ajouter un role superieur a votre role le plus haut.`);
+            if(message.guild.me.roles.highest.comparePositionTo(roleToAdd) <= 0) return console.log(`Je n'ai pas un role sufisant pour vous attribuer ce role`)
+            member.roles.add(roleToAdd)
+         }
+       }
+      })
+
+ /* console.log('RR add')
+  const guild = messageReaction.message.channel.guild
   const member = guild.members
-  console.log(member)*/
-  /*message = await  messageReaction.message;
+  console.log(member)
+  message = await  messageReaction.message;
   //member = await message.guild.members.cache.get(`${user.id}`);
   member = client.resolveMember(message.guild,`611468402263064577`)
     
-    
-
+    */
+/*
  return console.log(srr)
   emoji = messageReaction.emoji.name;
   emojiID = messageReaction.emoji.id;
--- events/reactions/messageReactionRemove.js
+++ events/reactions/messageReactionRemove.js
@@ -1,4 +1,25 @@
 module.exports = async (client, messageReaction, user) => {
+  const message = messageReaction.message;
+  const member = message.guild.members.cache.get(user.id);
+  const emojiID = messageReaction.emoji.id;
+  const emoji = messageReaction.emoji.name;
+  const channel = message.guild.channels.cache.find(c => c.id === '708593997890977823');
+  const roleun = message.guild.roles.cache.get("713757081966215269");
+  const roledeux = message.guild.roles.cache.get("713757111678664845");
+  const settings = await client.getGuild(message.guild);
+  if (member.user.bot) return;
+  settings.reactionroles.forEach(element => {
+
+    if(element.messageID === `${message.id}` && element.channelID === `${message.channel.id}`){
+      if(element.emoji == `${emojiID}`|| element.emoji == `${emoji}`){      //console.log(element)
+        let roleToRem = message.guild.roles.cache.get(`${element.roleID}`)
+        if(member.roles.highest.comparePositionTo(roleToRem) <= 0)return console.log(`Vous ne pouvez pas enlever un role superieur a votre role le plus haut.`);
+        if(message.guild.me.roles.highest.comparePositionTo(roleToRem) <= 0) return console.log(`Je n'ai pas un role sufisant pour vous enlever ce role`)
+        member.roles.remove(roleToRem)
+      }
+    }
+   })
+  //console.log('RR rem')
   /*const message = messageReaction.message;
   const member = await message.guild.members.cache.get(user.id);
   const emojiID = messageReaction.emoji.id;
